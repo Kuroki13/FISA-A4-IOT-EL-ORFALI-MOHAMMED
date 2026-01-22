60e26959781f998ab6adaa188a7f39d3ea3275c4
KisaPulga <55848121+KisaPulga@users.noreply.github.com>
2026-01-22 17:59:30 +0100

    Clean / mise en page

--- 01_Sensor_Device/Tx_Station/src/Sensor_Pressure.h
+++ 01_Sensor_Device/Tx_Station/src/Sensor_Pressure.h
@@ -9,44 +9,48 @@ const int pressureSensorPin = A0; // Pin analogique pour le capteur de pression
 // Variable globales
 float pressionActuelle = 0.0;
 
-// Les fonctions de gestion de la pression 
+// Les fonctions de gestion de la pression
 /**
-* 1. ACQUISITION & FILTRAGE
-* Lit le capteur X fois et fait une moyenne pour supprimer le bruit.
-*/
+ * 1. ACQUISITION & FILTRAGE
+ * Lit le capteur X fois et fait une moyenne pour supprimer le bruit.
+ */
 
-int lireValeurBrutePression(){
+int lireValeurBrutePression()
+{
 	long somme = 0;
 	int nombreEchantillons = 10; // On fait la moyenne de 10 lectures
 
-	for (int i = 0; i < nombreEchantillons; i++){
+	for (int i = 0; i < nombreEchantillons; i++)
+	{
 		somme += analogRead(pressureSensorPin);
-		delay(2); 
+		delay(2);
 	}
 
 	return somme / nombreEchantillons;
- }
-
-/**
-* 2. CONVERSION (Calibration)
-* Transforme la valeur 0-1023 en hPa.
-* Pour le SPX3058D, il faudra ajuster les valeurs min/max.
-*/
-
-float convertirEnPression(int valeurBrute){
-
-	// --- FORMULE REELLE (A calibrer avec un baromètre de référence) ---
-  	// Exemple théorique : 0.5V = 0 hPa, 4.5V = 2000 hPa
-  	// Sur Arduino R4 (ADC 10-bit par défaut) :
-  	// Voltage = (valeurBrute / 1023.0) * 5.0;
-  	// Pression = (Voltage - Offset) * ScaleFactor;
-
-  	// Pour une station spatiale, on veut mesurer de 0 (Vide) à ~1200 hPa (Max sécurité)
-  	float hpa = (valeurBrute / 1023.0) * 1200.0; 
-  	return hpa;
 }
 
-float getDataPression(){
+/**
+ * 2. CONVERSION (Calibration)
+ * Transforme la valeur 0-1023 en hPa.
+ * Pour le SPX3058D, il faudra ajuster les valeurs min/max.
+ */
+
+float convertirEnPression(int valeurBrute)
+{
+
+	// --- FORMULE REELLE (A calibrer avec un baromètre de référence) ---
+	// Exemple théorique : 0.5V = 0 hPa, 4.5V = 2000 hPa
+	// Sur Arduino R4 (ADC 10-bit par défaut) :
+	// Voltage = (valeurBrute / 1023.0) * 5.0;
+	// Pression = (Voltage - Offset) * ScaleFactor;
+
+	// Pour une station spatiale, on veut mesurer de 0 (Vide) à ~1200 hPa (Max sécurité)
+	float hpa = (valeurBrute / 1023.0) * 1200.0;
+	return hpa;
+}
+
+float getDataPression()
+{
 	float raw = 0.0;
 
 	// Acquisition & Filtrage
@@ -55,10 +59,10 @@ float getDataPression(){
 	pressionActuelle = convertirEnPression(raw);
 	Serial.print("Pression actuelle (hPa): ");
 	Serial.println(pressionActuelle);
-	
+
 	return pressionActuelle;
 
 	delay(1000);
 }
 
-#endif // SENSOR_PRESSURE_H 
+#endif // SENSOR_PRESSURE_H
--- 01_Sensor_Device/Tx_Station/src/Sensor_TempHum.h
+++ 01_Sensor_Device/Tx_Station/src/Sensor_TempHum.h
@@ -3,25 +3,30 @@
 
 Adafruit_BME280 bme;
 
-void initBme280(){
-
+void initBme280()
+{
     unsigned status;
     // Initialisation capteur BME280 et vérification connexion
     status = bme.begin(0x76);
-    if(!status){
+    if (!status)
+    {
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        while (1) delay(10);
+        Serial.print("SensorID was: 0x");
+        Serial.println(bme.sensorID(), 16);
+        while (1)
+            delay(10);
     }
 }
 
-float getTemp() {
+float getTemp()
+{
     Serial.print("Température actuelle (°C): ");
     Serial.println(bme.readTemperature());
     return bme.readTemperature();
 }
 
-float getHum() {
+float getHum()
+{
     Serial.print("Humidité actuelle (%): ");
     Serial.println(bme.readHumidity());
     return bme.readHumidity();
