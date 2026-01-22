36304e69680b36932450ec63d3acc79d256e0c02
KisaPulga <55848121+KisaPulga@users.noreply.github.com>
2026-01-22 17:58:37 +0100

    Ajout température + modification MAIN
    - ajout récupération température
    - ajout récupération humidité
    - modification main : méthode globale et modulable pour les requetes MQTT

--- 01_Sensor_Device/Tx_Station/src/Sensor_Pressure.h
+++ 01_Sensor_Device/Tx_Station/src/Sensor_Pressure.h
@@ -1,37 +1,21 @@
 #ifndef SENSOR_PRESSURE_H
 #define SENSOR_PRESSURE_H
 
-
-
 #include <Arduino.h>
 
 // Configuration du capteur pression
 const int pressureSensorPin = A0; // Pin analogique pour le capteur de pression
 
-// Seuille de sécurité (ECLSS) pour la pression (en hPa)
-const float PRESSION_NOMINALE = 1013.25; // Pression standard au niveau de la mer
-const float PRESSION_SEUIL_BAS = 980.0;  // Seuil bas de pression (fuite possible)
-const float PRESSION_SEUIL_HAUT = 1050.0; // Seuil haut de pression (surpression possible)
-
-
 // Variable globales
 float pressionActuelle = 0.0;
 
-// -- Simulation -- Variable globale qui stocke notre "Pression Virtuelle"
-float pressionSimulee = PRESSION_NOMINALE;
-
-bool alarmeActivee = false;
-
-
 // Les fonctions de gestion de la pression 
+/**
+* 1. ACQUISITION & FILTRAGE
+* Lit le capteur X fois et fait une moyenne pour supprimer le bruit.
+*/
 
-
- /**
- * 1. ACQUISITION & FILTRAGE
- * Lit le capteur X fois et fait une moyenne pour supprimer le bruit.
- */
-
- int lireValeurBrutePression(){
+int lireValeurBrutePression(){
 	long somme = 0;
 	int nombreEchantillons = 10; // On fait la moyenne de 10 lectures
 
@@ -43,13 +27,13 @@ bool alarmeActivee = false;
 	return somme / nombreEchantillons;
  }
 
- /**
- * 2. CONVERSION (Calibration)
- * Transforme la valeur 0-1023 en hPa.
- * Pour le SPX3058D, il faudra ajuster les valeurs min/max.
- */
+/**
+* 2. CONVERSION (Calibration)
+* Transforme la valeur 0-1023 en hPa.
+* Pour le SPX3058D, il faudra ajuster les valeurs min/max.
+*/
 
- float convertirEnPression(int valeurBrute){
+float convertirEnPression(int valeurBrute){
 
 	// --- FORMULE REELLE (A calibrer avec un baromètre de référence) ---
   	// Exemple théorique : 0.5V = 0 hPa, 4.5V = 2000 hPa
@@ -60,19 +44,6 @@ bool alarmeActivee = false;
   	// Pour une station spatiale, on veut mesurer de 0 (Vide) à ~1200 hPa (Max sécurité)
   	float hpa = (valeurBrute / 1023.0) * 1200.0; 
   	return hpa;
- }
-
-
-
-
-
- 
- void initCapteurPression() {
-	Serial.begin(9600);
-	pinMode(pressureSensorPin, INPUT);
-	
-
-	
 }
 
 float getDataPression(){
@@ -85,13 +56,9 @@ float getDataPression(){
 	Serial.print("Pression actuelle (hPa): ");
 	Serial.println(pressionActuelle);
 	
-
-
-	
 	return pressionActuelle;
 
 	delay(1000);
-
 }
 
 #endif // SENSOR_PRESSURE_H 
